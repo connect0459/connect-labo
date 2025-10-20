a940f251c17125896a70a0c5bc2797a2461f7afa
connect0459 <connect0459@gmail.com>
2025-10-20 16:48:18 +0900

    feat: Iteration 10が完了

--- workspaces/rust/rust-tcp-stack/src/ipv4.rs
+++ workspaces/rust/rust-tcp-stack/src/ipv4.rs
@@ -1,5 +1,18 @@
 use std::net::Ipv4Addr;
 
+pub struct Ipv4Packet<'a> {
+    data: &'a [u8],
+}
+
+impl<'a> Ipv4Packet<'a> {
+    pub fn new(data: &'a [u8]) -> Option<Self> {
+        if data.len() < 20 {
+            return None;
+        }
+        Some(Ipv4Packet { data })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
