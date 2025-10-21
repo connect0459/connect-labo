b7546422c5ab8824d60ec9b5ef3fea50463e89d4
connect0459 <connect0459@gmail.com>
2025-10-21 13:20:41 +0900

    feat: Iteration 21-23が完了

--- workspaces/rust/rust-tcp-stack/src/tcp.rs
+++ workspaces/rust/rust-tcp-stack/src/tcp.rs
@@ -17,6 +17,14 @@ impl<'a> TcpPacket<'a> {
     pub fn destination_port(&self) -> u16 {
         u16::from_be_bytes([self.data[2], self.data[3]])
     }
+
+    pub fn sequence_number(&self) -> u32 {
+        u32::from_be_bytes([self.data[4], self.data[5], self.data[6], self.data[7]])
+    }
+
+    pub fn acknowledgment_number(&self) -> u32 {
+        u32::from_be_bytes([self.data[8], self.data[9], self.data[10], self.data[11]])
+    }
 }
 
 #[cfg(test)]
@@ -47,5 +55,23 @@ mod tests {
 
         let packet = TcpPacket::new(&data).unwrap();
         assert_eq!(packet.destination_port(), 8080);
+    }
+
+    #[test]
+    fn シーケンス番号を取得できる() {
+        let mut data = [0u8; 20];
+        data[4..8].copy_from_slice(&[0x00, 0x00, 0x00, 0x64]); // 100
+
+        let packet = TcpPacket::new(&data).unwrap();
+        assert_eq!(packet.sequence_number(), 100);
+    }
+
+    #[test]
+    fn 確認応答番号を取得できる() {
+        let mut data = [0u8; 20];
+        data[8..12].copy_from_slice(&[0x00, 0x00, 0x00, 0xc9]); // 201
+
+        let packet = TcpPacket::new(&data).unwrap();
+        assert_eq!(packet.acknowledgment_number(), 201);
     }
 }
